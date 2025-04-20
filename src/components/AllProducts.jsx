f7c1f34283d6dcb7383754fc6e710eca13f20b85
TemirovaAzima <temirovaazima9@gmail.com>
2025-04-21 00:16:49 +0500

    AllProducts with destructing

--- src/components/AllProducts.jsx
+++ src/components/AllProducts.jsx
@@ -2,17 +2,17 @@ import React from 'react'
 import {useGetAllProductQuery} from "../app/services/dummydata.js";
 
 const AllProducts = () => {
-    const response = useGetAllProductQuery()
-    console.log(response)
-    if(response.isError){
+    const {data:myData,isError:fetchError,isLoading:loadingState} = useGetAllProductQuery()
+
+    if(fetchError){
         return <h1>Error</h1>
     }
-    if(response.isLoading){
+    if(loadingState){
         return <h1>Loading...</h1>
     }
     return (
         <div>
-            {response.data?.products.map((product) => (
+            {myData?.products.map((product) => (
                 <h1 key={product.id}>{product.title}</h1>
             ))}
         </div>
